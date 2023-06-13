6637ddb232f8796fb3da6069e69872e5461835b3
Menu <109733313+Khampasert9328@users.noreply.github.com>
2023-06-13 16:49:34 +0700

    commit

--- src/controllers/addinfocharging_controllers.js
+++ src/controllers/addinfocharging_controllers.js
@@ -1,13 +1,21 @@
 const Addinfomodels = require("../models/addinfocharging/add_info_charging_models");
+const aws = require("aws-sdk");
+const { json } = require("express");
+const path = require("path");
+const { S3_ENDPOINT, BUCKET_NAME } = process.env;
+
+const spacesEndpoint = new aws.Endpoint(S3_ENDPOINT);
+
+const s3 = new aws.S3({
+  endpoint: spacesEndpoint,
+});
 
 exports.insertInfoCharging = async (req, res) => {
   try {
     const {
       name,
-      imagecpn,
       amount,
       constainner,
-      pictureplace,
       province,
       district,
       village,
@@ -16,54 +24,78 @@ exports.insertInfoCharging = async (req, res) => {
       lat_location,
       lng_lacation,
     } = req.body;
-    console.log("req", req.body);
-    const data = new Addinfomodels({
-      name: name,
-      imagecpn: imagecpn,
-      amount: amount,
-      pictureplace: pictureplace,
-      province: province,
-      district: district,
-      village: village,
-      nameplace: nameplace,
-      lat_location: lat_location,
-      lng_lacation: lng_lacation,
-    });
-    let containerArray = [];
-    let facilitiesArray = [];
-    if (constainner) {
-      for (let index = 0; index < constainner.length; index++) {
-        containerArray.push({
-          count: `ຕູ້ທີ${index + 1}`,
-          brand: constainner[index].brand,
-          generation: constainner[index].generation,
-          model: constainner[index].model,
-          type_charge: constainner[index].type_charge,
-        });
-      }
-      data.constainner = containerArray;
-    }
+    const file = req.file;
+    if (!file) return res.json({ msg: "no file" });
+    const fileType = path.extname(file.originalname).toLowerCase();
+    if (fileType == ".png" || fileType == ".jpg" || fileType == ".jpeg") {
+      // create file name
+      const randomChar = Math.random().toString(36).substring(7);
+      const time = new Date().getTime();
+      const fileName = `${randomChar}${time}` + `${fileType}`;
 
-    if (facilities) {
-      for (let index = 0; index < facilities.length; index++) {
-        facilitiesArray.push({
-          facilitie: facilities[index].facilitie,
-        });
-      }
-      data.facilities = facilitiesArray;
-    }
+      const uploadedObject = await s3
+        .putObject({
+          ACL: "public-read",
+          Bucket: BUCKET_NAME + "/Upload",
+          Key: fileName,
+          Body: file.buffer,
+          ContentType: file.mimetype,
+        })
+        .promise();
+      console.log(uploadedObject);
 
-    if (!data) {
-      res.status(404).json({
-        satatus_cod:404,
-        message: "ເກີດຂໍ້ຜິດພາດ",
+      const urlImage = `https://ad-bucket.sgp1.digitaloceanspaces.com/Upload/${fileName}`;
+      const data = new Addinfomodels({
+        name: name,
+        imagecpn: urlImage,
+        amount: amount,
+        pictureplace: urlImage,
+        province: province,
+        district: district,
+        village: village,
+        nameplace: nameplace,
+        lat_location: lat_location,
+        lng_lacation: lng_lacation,
       });
+      let containerArray = [];
+      let facilitiesArray = [];
+      if (constainner) {
+        for (let index = 0; index < constainner.length; index++) {
+          containerArray.push({
+            count: `ຕູ້ທີ${index + 1}`,
+            brand: constainner[index].brand,
+            generation: constainner[index].generation,
+            model: constainner[index].model,
+            type_charge: constainner[index].type_charge,
+          });
+        }
+        data.constainner = containerArray;
+      }
+
+      if (facilities) {
+        for (let index = 0; index < facilities.length; index++) {
+          facilitiesArray.push({
+            facilitie: facilities[index].facilitie,
+          });
+        }
+        data.facilities = facilitiesArray;
+      }
+
+      if (!data) {
+        res.status(404).json({
+          satatus_cod: 404,
+          message: "ເກີດຂໍ້ຜິດພາດ",
+        });
+      }
+      await data.save();
+      res.status(201).json({
+        status_code: 201,
+        message: "ບັນທຶກຂໍ້ມູນສຳເລັດ",
+        data: data,
+      });
+    } else {
+      return res.json({ msg: "allow only png, jpg, jpeg" });
     }
-    await data.save();
-    res.status(201).json({
-      message: "ບັນທຶກຂໍ້ມູນສຳເລັດ",
-      data: data,
-    });
   } catch (error) {
     console.log(error);
   }
@@ -74,7 +106,7 @@ exports.getInfochargAll = async (req, res) => {
     const data = await Addinfomodels.find();
     if (!data) {
       res.status(404).json({
-        satatus_cod:404,
+        satatus_cod: 404,
         message: "ກາລຸນາລອງໃໝ່ອີກຄັ້ງ",
       });
     }
@@ -90,18 +122,121 @@ exports.deletebyid = async (req, res, next) => {
     const data = await Addinfomodels.findById(id);
     console.log("data", data);
     if (!data) {
-      res.status(404).json({ 
-        satatus_cod:404,
-        message: "ບໍ່ມີໄອດີນີ້ໃນລະບົບ" });
+      res.status(404).json({
+        satatus_cod: 404,
+        message: "ບໍ່ມີໄອດີນີ້ໃນລະບົບ",
+      });
     } else {
       await data.deleteOne({ _id: id });
       res.status(200).json({ message: "ລົບຂໍ້ມູນໃນລະບົບສຳເລັດແລ້ວ" });
     }
   } catch (error) {
     console.log(error);
-    res.status(500).json({ 
-      satatus_cod:500,
-      message: "Server error" 
+    res.status(500).json({
+      satatus_cod: 500,
+      message: "Server error",
     });
+  }
+};
+
+exports.updateInfochargbyid = async (req, res, next) => {
+  try {
+    const {
+      name,
+      //imagecpn,
+      amount,
+      constainner,
+      pictureplace,
+      province,
+      district,
+      village,
+      nameplace,
+      facilities,
+      lat_location,
+      lng_lacation,
+    } = req.body;
+    console.log("body:",req.body);
+    const { id } = req.params;
+    const file = req.file;
+    if (!file) return res.json({ msg: "no file" });
+    console.log(req.body);
+    const updatedata = await Addinfomodels.findById(
+      { _id: id } & { del: false }
+    );
+    if (!updatedata) {
+      return res.status(404).json({ message: "ບໍ່ມີໄອດີນີ້ໃນລະບົບ" });
+    } else {
+      const fileType = path.extname(file.originalname).toLowerCase();
+      if (fileType == ".png" || fileType == ".jpg" || fileType == ".jpeg") {
+        // create file name
+        const randomChar = Math.random().toString(36).substring(7);
+        const time = new Date().getTime();
+        const fileName = `${randomChar}${time}` + `${fileType}`;
+
+        const uploadedObject = await s3
+          .putObject({
+            ACL: "public-read",
+            Bucket: BUCKET_NAME + "/Upload",
+            Key: fileName,
+            Body: file.buffer,
+            ContentType: file.mimetype,
+          })
+          .promise();
+        console.log(uploadedObject);
+
+        const urlImage = `https://ad-bucket.sgp1.digitaloceanspaces.com/Upload/${fileName}`;
+
+        const data = await Addinfomodels.findByIdAndUpdate(
+          { _id: id },
+          {
+            name: name,
+            imagecpn: urlImage,
+            amount: amount,
+            pictureplace: urlImage,
+            province: province,
+            district: district,
+            village: village,
+            nameplace: nameplace,
+            lat_location: lat_location,
+            lng_lacation: lng_lacation,
+          }
+        );
+        let containerArray = [];
+        let facilitiesArray = [];
+        if (constainner) {
+          for (let index = 0; index < constainner.length; index++) {
+            containerArray.set({
+              brand: constainner[index].brand,
+              generation: constainner[index].generation,
+              model: constainner[index].model,
+              type_charge: constainner[index].type_charge,
+            });
+          }
+          data.constainner = containerArray;
+        }
+
+        if (facilities) {
+          for (let index = 0; index < facilities.length; index++) {
+            facilitiesArray.set({
+              facilitie: facilities[index].facilitie,
+            });
+          }
+          data.facilities = facilitiesArray;
+        }
+
+        if (!data) {
+          res.status(404).json({ message: "ບໍ່ສາມາດແກ້ໄຂໄດ້ ລອງໃໝ່ອີກຄັ້ງ" });
+        }
+        res.status(200).json({ 
+          message: "ອັບເດດຂໍ້ມູນສຳເລັດ",
+          data:data
+        });
+      } else {
+        return res.json({ msg: "allow only png, jpg, jpeg" });
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500),json({message: "Server error"})
   }
 };
--- src/routes/addinfocharging/add_info_route.js
+++ src/routes/addinfocharging/add_info_route.js
@@ -1,14 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const upload= multer()
+
 
 //middleware
-const passportJWT = require('../../middleware/passport-jwt')
+const passportJWT = require("../../middleware/passport-jwt");
 
-const insetinfocharging = require('../../..//src/controllers/addinfocharging_controllers');
+const insetinfocharging = require("../../..//src/controllers/addinfocharging_controllers");
 
-router.post('/insertinfocharg',[passportJWT.isLogin],insetinfocharging.insertInfoCharging)
-router.get('/getinfochargall',[passportJWT.isLogin],insetinfocharging.getInfochargAll)
-router.delete('/deleteinfochargall/:id',[passportJWT.isLogin], insetinfocharging.deletebyid)
+router.post(
+  "/insertinfocharg",
+  [passportJWT.isLogin],upload.fields([{name:"imagecpn", maxCount:1},{name:"pictureplace", maxCount:1}]),
+  insetinfocharging.insertInfoCharging
+);
+router.get(
+  "/getinfochargall",
+  [passportJWT.isLogin],
+  insetinfocharging.getInfochargAll
+);
+router.delete(
+  "/deleteinfochargall/:id",
+  [passportJWT.isLogin],
+  insetinfocharging.deletebyid
+);
 
+router.put('/updateinfocharg/:id', insetinfocharging.updateInfochargbyid)
 
-module.exports= router
+module.exports = router;
